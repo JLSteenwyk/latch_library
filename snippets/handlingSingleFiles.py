3788333b3d2876c277c7d69a85f267544778f5e7
George <96872843+GeOdette@users.noreply.github.com>
2022-06-07 17:42:27 +0300

    Create handlingSingleFiles.py

---
+++ snippets/handlingSingleFiles.py
@@ -0,0 +1,13 @@
+import os
+import shutil
+# Print output in a tool that prints output in the console
+	with open("file.txt", "w") as f:
+        subprocess.run(__command__, stdout=f)
+    
+# If the tool gives only one file
+os.mkdir("/root/foo") # Create your preffered directory
+ _comamnd_cmd[command, Check=True]
+ shutil.move("/root/file.txt", "/root/foo/file.txt") # os.remane() can be used here. Note that in all use case (shutil.move or os.rename(),
+                                                     # the command must end with the file e.g file.txt)
+
+  return LatchDir(str("foo"), out_dir.remote_path) # You can now use Latch syntax to return your file. 
